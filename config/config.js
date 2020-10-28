216ed2dc05eb8462ac9d0af4e50573ca0da05797
Jon Penwood <jonp@polarity.io>
2020-10-28 17:09:15 -0500

    Updated rejectUnauthorized property in config.js

--- config/config.js
+++ config/config.js
@@ -1,106 +1,95 @@
 module.exports = {
-    /**
-     * Name of the integration which is displayed in the Polarity integrations user interface
-     *
-     * @type String
-     * @required
-     */
-    name: "Google Translate",
-    /**
-     * The acronym GT appears in the notification window when information from this integration
-     * is displayed.  Note GT the acronym is included as part of each "tag" in the summary information
-     * for the integration.  As a result, it is best to keep it to 4 or less cGTracters.  The casing used
-     * here will be carried forward into the notification window.
-     *
-     * @type String
-     * @required
-     */
-    acronym: "GT",
-    /**
-     * Description for this integration which is displayed in the Polarity integrations user interface
-     *
-     * @type String
-     * @optional
-     */
-    description: "Googles free translation service ",
-    entityTypes: [],
-    customTypes:[
-        {
-          key: 'translate',
-          regex: /[\s\S]*/
-        }
-      ],
+  /**
+   * Name of the integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @required
+   */
+  name: 'Google Translate',
+  /**
+   * The acronym GT appears in the notification window when information from this integration
+   * is displayed.  Note GT the acronym is included as part of each "tag" in the summary information
+   * for the integration.  As a result, it is best to keep it to 4 or less cGTracters.  The casing used
+   * here will be carried forward into the notification window.
+   *
+   * @type String
+   * @required
+   */
+  acronym: 'GT',
+  /**
+   * Description for this integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @optional
+   */
+  description: 'Googles free translation service ',
+  entityTypes: [],
+  customTypes: [
+    {
+      key: 'translate',
+      regex: /[\s\S]*/
+    }
+  ],
 
-    /**
-     * An array of style files (css or less) GT will be included for your integration. Any styles specified in
-     * the below files can be used in your custom template.
-     *
-     * @type Array
-     * @optional
-     */
-    "styles": [
-        "./styles/gt.less",
-    ],
-    /**
-     * Provide custom component logic and template for rendering the integration details block.  If you do not
-     * provide a custom template and/or component then the integration will display data as a table of key value
-     * pairs.
-     *
-     * @type Object
-     * @optional
-     */
-    block: {
-        component: {
-            file: "./components/gt-block.js"
-        },
-        template: {
-            file: "./templates/gt-block.hbs"
-        }
+  /**
+   * An array of style files (css or less) GT will be included for your integration. Any styles specified in
+   * the below files can be used in your custom template.
+   *
+   * @type Array
+   * @optional
+   */
+  styles: ['./styles/gt.less'],
+  /**
+   * Provide custom component logic and template for rendering the integration details block.  If you do not
+   * provide a custom template and/or component then the integration will display data as a table of key value
+   * pairs.
+   *
+   * @type Object
+   * @optional
+   */
+  block: {
+    component: {
+      file: './components/gt-block.js'
     },
-    summary: {
-        component: {
-            file: "./components/gt-summary.js"
-        },
-        template: {
-            file: "./templates/gt-summary.hbs"
-        }
+    template: {
+      file: './templates/gt-block.hbs'
+    }
+  },
+  summary: {
+    component: {
+      file: './components/gt-summary.js'
     },
-    request: {
-        // Provide the path to your certFile. Leave an empty string to ignore this option.
-        // Relative paths are relative to the GT integration's root directory
-        cert: '',
-        // Provide the path to your private key. Leave an empty string to ignore this option.
-        // Relative paths are relative to the GT integration's root directory
-        key: '',
-        // Provide the key passphrase if required.  Leave an empty string to ignore this option.
-        // Relative paths are relative to the GT integration's root directory
-        passphrase: '',
-        // Provide the Certificate Authority. Leave an empty string to ignore this option.
-        // Relative paths are relative to the GT integration's root directory
-        ca: '',
-        // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
-        // the url parameter (by embedding the auth info in the uri)
-        proxy: ''
-    },
-    logging: {
-        level: 'info',  //trace, debug, info, warn, error, fatal
-    },
-    /**
-     * Options GT are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
-     * as an array of option objects.
-     *
-     * @type Array
-     * @optional
-     */
-    "options": [
-        {
-            "key": "apiKey",
-            "name": "API Key",
-            "description": "Google Translate API Key",
-            "default": "",
-            "type": "text",
-            "userCanEdit": true,
-            "adminOnly": false
-        }
-    ]
+    template: {
+      file: './templates/gt-summary.hbs'
+    }
+  },
+  request: {
+    cert: '',
+    key: '',
+    passphrase: '',
+    ca: '',
+    proxy: '',
+    rejectUnauthorized: true
+  },
+  logging: {
+    level: 'info'
+  },
+  /**
+   * Options GT are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
+   * as an array of option objects.
+   *
+   * @type Array
+   * @optional
+   */
+  options: [
+    {
+      key: 'apiKey',
+      name: 'API Key',
+      description: 'Google Translate API Key',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    }
+  ]
 };
